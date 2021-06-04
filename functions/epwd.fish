0cdf93fe548f95d7c16be511efe94656ef9e1ad2
Robert Westman <robert@byteflux.io>
2021-06-05 01:05:05 +0200

    Adapts the plugin for 3.2.2

--- functions/epwd.fish
+++ functions/epwd.fish
@@ -17,7 +17,7 @@ function epwd -d "Output the current working directory relative to environment v
     set path_test (pwd | grep '^'$p)
     if test "$path_test" != ""
       set tmp_len (string length $p)
-      if test (math $tmp_len" > "$path_len) = "1"
+      if test $tmp_len -gt $path_len
         set path_matched $p
         set path_env_index $i
         set path_len $tmp_len
@@ -33,4 +33,3 @@ function epwd -d "Output the current working directory relative to environment v
     pwd
   end
 end
-
